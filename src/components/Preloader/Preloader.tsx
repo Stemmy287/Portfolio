7fef115c144f770a7e563e53f8eecb28fca425bd
Stemmy287 <steem287@gmail.com>
2023-02-27 18:05:19 +0300

    feat: refactor preloader

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -8,12 +8,20 @@ export const Preloader = () => {
       setIsPreloader(true)
     },500)
 
+    const body = document.body
+
+    if (!isPreloader) {
+      body.style.overflow = 'hidden'
+    } else {
+      body.style.overflow = 'auto'
+    }
+
   return (
-    <div className={isPreloader ? `${s.preloaderContainer} ${s.active}` : s.preloaderContainer}>
-      <div className={s.loading}>
-        <Oval color={'#29a587'} secondaryColor={'#29a587'}/>
+      <div className={isPreloader ? `${s.preloaderContainer} ${s.active}` : s.preloaderContainer}>
+        <div className={s.loading}>
+          <Oval color={'#29a587'} secondaryColor={'#29a587'}/>
+        </div>
       </div>
-    </div>
   );
 };
 
